6245f4807ba66bcecce1c58f2acb1fa6cd399e59
Peter <prchillemi+github@gmail.com>
2016-07-25 21:35:57 +0800

    added favicon, added new api call to get all public channels

--- src/web/routes/api.js
+++ src/web/routes/api.js
@@ -2,7 +2,7 @@ import CyTubeUtil from '../../utilities';
 import * as HTTPStatus from '../httpstatus';
 
 export default function initialize(app, channelIndex) {
-    app.get('/api/:channel.json', (req, res) => {
+    app.get('/api/channel/:channel.json', (req, res) => {
         if (!req.params.channel || !CyTubeUtil.isValidChannelName(req.params.channel)) {
             return res.status(HTTPStatus.NOT_FOUND).json({
                 error: `Channel "${req.params.channel}" does not exist.`
@@ -21,6 +21,23 @@ export default function initialize(app, channelIndex) {
                     error: `Channel "${req.params.channel}" does not exist.`
                 });
             }
+        }).catch(err => {
+            Logger.errlog.log(err.stack);
+            return res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({
+                error: err.message
+            });
+        });
+    });
+
+    app.get('/api/channels.json', (req, res) => {
+        
+        channelIndex.listPublicChannels().then((channels) => {
+            var results = {
+                "total_channels": channels.length,
+                "channels": channels,
+            };
+
+            res.json(results);
         }).catch(err => {
             Logger.errlog.log(err.stack);
             return res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({
